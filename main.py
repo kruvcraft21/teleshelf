0cac2fb24539ccbb2666f21198ad245c29105949
egor <kruvcraft@yandex.ru>
2026-07-10 08:56:18 +0500

    Refactor main.py to integrate user and other routers, and implement Uvicorn server for handling requests. Create monolith

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from aiogram import Bot, Dispatcher
 
 from config import load_config, Config
 from handlers.chats import chats_router
+from handlers.other import other_router
+from handlers.user import user_router
 from database.db import Database
+
+from hydrogram import Client
+
+from reader import reader
+import uvicorn
 
 import logging
 import asyncio
@@ -26,46 +33,32 @@ async def main():
         token=config.bot.token,
     )
     dp = Dispatcher()
+    hy_client = Client("tg_reader",api_id=config.tg_api.api_id, api_hash=config.tg_api.api_hash, bot_token=config.bot.token)
+    reader.state.bot = hy_client
 
     # Инициализируем "базу данных"
     db = await Database.create(config.database)
-
-    # Сохраняем готовую книгу и "базу данных" в \`workflow_data\`
-    dp.workflow_data.update(db=db)
-
     # Регистрируем роутеры в диспетчере
     dp.include_router(chats_router)
+    dp.include_router(user_router)
+
+    if config.log.level in ["DEBUG", "INFO"]:
+        dp.include_router(other_router)
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+
+    uvicorn_config = uvicorn.Config(host="127.0.0.1", port=8000, log_level="info", workers=4, app=reader)
+    uvicorn_server = uvicorn.Server(uvicorn_config)
+
+    try:
+        await hy_client.start()
+        await asyncio.gather(uvicorn_server.serve(),
+                             dp.start_polling(bot, db=db, hy_client=hy_client))
+
+    finally:
+        await hy_client.stop()
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-# async def main():
-#     # Загружаем конфиг в переменную config
-#     config: Config = load_config()
-#
-#     # Задаём базовую конфигурацию логирования
-#     logging.basicConfig(
-#         level=logging.getLevelName(level=config.log.level),
-#         format=config.log.format,
-#     )
-#     # Выводим в консоль информацию о начале запуска бота
-#     logger.info("Starting bot")
-#
-#     # Инициализируем бот и диспетчер
-#     bot = Bot(
-#         token=config.bot.token,
-#     )
-#     dp = Dispatcher()
-#
-#     # Пропускаем накопившиеся апдейты и запускаем polling
-#     await bot.delete_webhook(drop_pending_updates=True)
-#     await dp.start_polling(bot)
-#
-# if __name__ == "__main__":
-#     asyncio.run(main())
 
