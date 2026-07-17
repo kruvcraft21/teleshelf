046918cd9f1e8a853c3549d8fd96937dea5a5c15
egor <kruvcraft@yandex.ru>
2026-07-17 18:59:46 +0500

    Refactor reader.html and webapp-worker.js to streamline script loading and enhance fullscreen functionality

--- static/js/webapp-worker.js
+++ static/js/webapp-worker.js
@@ -4,22 +4,6 @@ if (tg && tg.initData) {
     tg.ready();
     tg.expand();
 
-    const platform = tg.platform;
 
-    // Проверяем, что это мобильная платформа (iOS или Android)
-    if (platform === 'ios' || platform === 'android') {
-        tg.requestFullscreen();
-    }
+    tg.requestFullscreen();
 }
-
-// tg.MainButton.setText("Закрыть");
-// tg.MainButton.show();
-//
-// tg.onEvent("mainButtonClicked", () => {
-//     tg.sendData(JSON.stringify({
-//         user_id: "{{ user_id }}",
-//         book_id: "{{ book_id }}",
-//         action: "closed"
-//     }));
-//     tg.close();
-// });
