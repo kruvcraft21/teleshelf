476afe8768668ea24580c1df86b6dd99f3369a45
egor <kruvcraft@yandex.ru>
2026-07-20 21:43:27 +0500

    Refactor main.py to implement AppContext for better structure and add DataTransferJob for scheduled data transfers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from dataclasses import dataclass
+
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.redis import RedisStorage
-from aiogram.types import passport_data
 
 from config import load_config, Config
 from handlers.chats import chats_router
@@ -11,14 +12,16 @@ from database.db import Database
 from clients.hydroclient import HydroClient
 
 from reader import reader
-import uvicorn
 
 import logging
 import asyncio
 
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
+
+from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from schedulers.transfers import DataTransferJob
 
 logger = logging.getLogger(__name__)
 config: Config = load_config()
@@ -27,7 +30,15 @@ logging.basicConfig(
     format=config.log.format,
 )
 
-async def start_app():
+@dataclass(slots=True)
+class AppContext:
+    bot: Bot
+    db: Database
+    dp: Dispatcher
+    hydro: HydroClient
+    scheduler: AsyncIOScheduler
+
+async def start_app() -> AppContext:
     # Выводим в консоль информацию о начале запуска бота
     logger.info("Starting bot")
 
@@ -36,7 +47,7 @@ async def start_app():
         token=config.bot.token,
     )
     db = await Database.create(config.database, config.redis)
-    dp = Dispatcher(storage=RedisStorage(db.get_redis_poll(), data_ttl=300, state_ttl=300))
+    dp = Dispatcher(storage=RedisStorage(db.get_redis_poll(), data_ttl=1800, state_ttl=1800))
     dp.include_router(chats_router)
     dp.include_router(user_router)
     if config.log.level in ["DEBUG", "INFO"]:
@@ -46,7 +57,11 @@ async def start_app():
 
     hy_client = await HydroClient.start(name="tg_reader",api_id=config.tg_api.api_id, api_hash=config.tg_api.api_hash, bot_token=config.bot.token)
 
-    return hy_client, db, bot, dp
+    transfer_job = DataTransferJob(db, db.get_redis_wrap())
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(transfer_job.__call__, trigger="interval", minutes=1, max_instances=1)
+    scheduler.add_listener(transfer_job.handle_event, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+    return AppContext(bot=bot, db=db, dp=dp, hydro=hy_client, scheduler=scheduler)
 
 async def stop_app(client: HydroClient, db: Database):
     await client.stop()
@@ -55,17 +70,27 @@ async def stop_app(client: HydroClient, db: Database):
 
 @asynccontextmanager
 async def lifespan(fast_app: FastAPI):
-    hy_client, db, bot, dp = await start_app()
+    app_context = await start_app()
+    hy_client = app_context.hydro
+    dp = app_context.dp
+    db = app_context.db
+    bot = app_context.bot
     fast_app.state.bot = hy_client
     fast_app.state.db = db
     fast_app.state.polling_task = asyncio.create_task(dp.start_polling(bot, db=db, hy_client=hy_client, handle_signals=False))
+    app_context.scheduler.start()
     yield
     logger.info("Shutting down bot")
     await hy_client.stop()
     await db.close()
+    app_context.scheduler.shutdown()
 
 async def main():
-    hy_client, db, bot, dp = await start_app()
+    app_con: AppContext = await start_app()
+    hy_client: HydroClient = app_con.hydro
+    dp = app_con.dp
+    bot = app_con.bot
+    db = app_con.db
     try:
         await dp.start_polling(bot, db=db, hy_client=hy_client)
     finally:
