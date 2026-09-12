781c46f69d44562814154641bc69620339195fda
egor <kruvcraft@yandex.ru>
2026-09-12 21:52:18 +0500

    Refactor menu command setup to handle different scopes for users and groups

--- bot/keyboards/menu_commands.py
+++ bot/keyboards/menu_commands.py
@@ -1,7 +1,12 @@
+import asyncio
 import logging
 
 from aiogram import Bot
-from aiogram.types import BotCommand, BotCommandScopeDefault
+from aiogram.types import (
+    BotCommand,
+    BotCommandScopeAllGroupChats,
+    BotCommandScopeAllPrivateChats,
+)
 
 from bot.keyboards.model_commands import BotCommand as bc
 
@@ -9,14 +14,28 @@ logger = logging.getLogger(__name__)
 
 
 async def set_menu_commands(bot: Bot):
-    is_success = await bot.delete_my_commands()
-    logger.info(f"Deleted menu commands, success: {is_success}")
-    bot_commands = [
+    is_success = await asyncio.gather(
+        bot.delete_my_commands(),
+        bot.delete_my_commands(BotCommandScopeAllPrivateChats()),
+        bot.delete_my_commands(BotCommandScopeAllGroupChats()),
+    )
+    logger.info(f"Deleted menu commands, from default scope, success: {is_success[0]}")
+    logger.info(
+        f"Deleted menu commands, from all private chats scope, success: {is_success[1]}"
+    )
+    logger.info(
+        f"Deleted menu commands, from all group chats scope, success: {is_success[2]}"
+    )
+    user_commands = [
         BotCommand(command=bc.START, description="Начать работу с ботом"),
+    ]
+    group_commands = [
         BotCommand(
             command=bc.UPDATE,
             description="Обновить документы (Работает только в форумах)",
         ),
     ]
-    is_success = await bot.set_my_commands(bot_commands, scope=BotCommandScopeDefault())
-    logger.info(f"Set menu commands: {bot_commands}, success: {is_success}")
+    user_is_success = await bot.set_my_commands(user_commands, scope=BotCommandScopeAllPrivateChats())
+    logger.info(f"Set menu commands for users: {user_commands}, success: {user_is_success}")
+    group_us_success = await bot.set_my_commands(group_commands, scope=BotCommandScopeAllGroupChats())
+    logger.info(f"Set menu commands for groups: {group_commands}, success: {group_us_success}")
